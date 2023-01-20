b41b31a5d7292cdb2828419a275894ba9c89d2a8
Зарецкий Александр Александрович <aleksandr.zareckijj@vseinstrumenti.ru>
2023-01-20 23:48:05 +0300

    trying github actions

---
+++ internal/repo/repo_test.go
@@ -0,0 +1,28 @@
+package repo
+
+import (
+	"blynker/internal/model"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type RepoSuite struct {
+	suite.Suite
+}
+
+func TestRepoRun(t *testing.T) {
+	suite.Run(t, new(RepoSuite))
+}
+
+func (s *RepoSuite) TestRepo_Save() {
+	data := model.Sensor{
+		Temperature: 12,
+		Light:       12,
+		Movement:    false,
+		UpdatedAt:   time.Time{},
+	}
+	testRepo := new(Repo)
+	err := testRepo.Save(&data)
+	s.Require().NoError(err)
+}
