5bd70389ca16d33e643639dbb26ee3fb971548f0
soadmized <soadmized1@gmail.com>
2023-02-02 23:32:42 +0300

    new router

--- internal/api/api.go
+++ internal/api/api.go
@@ -18,59 +18,42 @@ type API struct {
 func New() *API {
 	srv := service.New()
 	a := API{service: &srv}
-	a.registerHandlers()
+	a.routeHandlers()
 
 	return &a
 }
 
-func (a *API) registerHandlers() {
-	a.HandleFunc("/", a.DisplayValues)
-	a.HandleFunc("/get_data", a.Get)
-	a.HandleFunc("/set_data", a.Save)
+func (a *API) routeHandlers() {
+	a.HandleFunc("/", a.Route)
 }
 
 func (a *API) Get(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		sensor := a.service.GetData()
-		makeResponse(w, sensor)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		makeResponse(w, "WRONG METHOD, USE POST")
-	}
+	sensor := a.service.GetData()
+	makeResponse(w, sensor)
 }
 
 func (a *API) Save(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		dec := json.NewDecoder(req.Body)
-		s := model.Sensor{}
+	dec := json.NewDecoder(req.Body)
+	s := model.Sensor{}
 
-		err := dec.Decode(&s)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Print(err)
-		}
-
-		s.UpdatedAt = time.Now()
-		err = a.service.SaveData(&s)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Print(err)
-		}
-		makeResponse(w, "DATA IS SAVED!")
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		makeResponse(w, "WRONG METHOD, USE POST")
+	err := dec.Decode(&s)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Print(err)
 	}
+
+	s.UpdatedAt = time.Now()
+	err = a.service.SaveData(&s)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Print(err)
+	}
+	makeResponse(w, "DATA IS SAVED!")
 }
 
 func (a *API) DisplayValues(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		data := a.service.GetData()
-		makeResponse(w, data)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		makeResponse(w, "WRONG METHOD, USE GET")
-	}
+	data := a.service.GetData()
+	makeResponse(w, data)
 }
 
 func makeResponse(w http.ResponseWriter, data any) {
